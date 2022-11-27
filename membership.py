3b473401a49c5aa049aa343079961d525c2399fe
dielawn <dielawnmerkill@gmail.com>
2022-11-27 04:55:23 -0700

    Augmented Assignment Operators.py

---
+++ membership.py
@@ -0,0 +1,13 @@
+x = [10,20,30,40,50*5]
+
+print("x is: ",x)
+
+print("Is 30 present in x?: ",(30 in x))
+
+print("Is 60 present in x?: ",(60 in x))
+
+print("Is 20 not present in x?: ",(30 not in x))
+
+print("Is 55 not present in x?: ",(55 not in x))
+
+print(500/2 in x)
---
+++ operators.py
@@ -0,0 +1,40 @@
+x = 10
+y = 5
+z = 10
+
+print("The value assigned to x variable is: ",x)
+
+print("The value assigned to y variable is: ",y)
+
+print("The value assigned to z variable is: ",z)
+
+print("The value after addition is: ",x+y)
+
+print("The value after subtraction is: ",x-y)
+
+print("The value after multiplication is: ",x*y)
+
+print("The value after division is: ",x/y)
+
+print("The value after floor division is: ",x//y)
+
+print("The modulus value after division is: ",x%y)
+
+print("The remainder value after division is: ",x**y)
+
+print("Is x greater than y?: ",x>y)
+
+print("Is x lesser than y?: ",x<y)
+
+print("Is x greater than or equals to z?: ",x>=z)
+
+print("Is x lesser than or equals to z?: ",x<=z)
+
+print("Is x equals to z?: ",x==z)
+
+print("Is x equals to y?: ",x==y)
+
+print("Is x not equals to y?: ",x!=y)
+
+print("Is x not equals to z?: ",x!=z)
+
