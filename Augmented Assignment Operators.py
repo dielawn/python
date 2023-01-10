f2dec99e3685a05cb3931170cfed7a53989eb499
dielawn <dielawnmerkill@gmail.com>
2023-01-10 05:40:12 -0700

    python stuff

---
+++ Augmented Assignment Operators.py
@@ -0,0 +1,19 @@
+x = 100
+y = 50
+
+print("Present value of x is: ",x)
+
+print("Present value of y is: ",y)
+
+x += y
+
+print("The value of x after performing x += y is: ",x)
+
+x-= y
+print("The value of x after performing x -= y is: ",x)
+
+x *= y
+print("The value of x after performing x *= y is: ",x)
+
+x /= y
+print("The value of x after performing x /= y is: ",x)
